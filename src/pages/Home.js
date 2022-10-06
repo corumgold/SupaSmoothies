52ae4c7bdddfbb15084dc22183ad46f091a358d0
Corum Gold <goldcorum@gmail.com>
2022-10-06 12:57:57 +0200

    Fetch smoothies from supabase and add styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,45 @@
 import supabase from "../config/supabaseClient";
+import { useEffect, useState } from "react";
+import SmoothieCard from "../components/SmoothieCard";
 
 const Home = () => {
-  console.log(supabase);
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        //name of table
+        .from("smoothies")
+        //all of the models
+        .select();
+
+      if (error) {
+        setFetchError("Could not fetch smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+    fetchSmoothies();
+  }, []);
 
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* order-by buttons */}
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
