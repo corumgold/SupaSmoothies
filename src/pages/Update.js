1949873c59103e79f060b35abee07d2bafc3186f
Corum Gold <goldcorum@gmail.com>
2022-10-06 14:33:40 +0200

    Add fetch single smoothie functionality

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,9 +1,73 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import supabase from "../config/supabaseClient";
+
 const Update = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [method, setMethod] = useState("");
+  const [rating, setRating] = useState("");
+
+  useEffect(() => {
+    const fetchSmoothie = async () => {
+      const { data, error } = await supabase
+        .from("smoothies")
+        .select()
+        //only select smoothie where "id" equals the id from useParams
+        .eq("id", id)
+        //telling supabase it's only going to be ONE model
+        .single();
+
+      if (error) {
+        //replace this route in the history with the home page
+        navigate("/", { replace: true });
+      }
+
+      if (data) {
+        setTitle(data.title);
+        setMethod(data.method);
+        setRating(data.rating);
+        console.log(data);
+      }
+    };
+
+    fetchSmoothie();
+  }, [id, navigate]);
+
   return (
     <div className="page update">
-      <h2>Update</h2>
-    </div>
-  )
-}
+      <form>
+        <label htmlFor="title">Title:</label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-export default Update
+        <label htmlFor="method">Method:</label>
+        <textarea
+          id="method"
+          value={method}
+          onChange={(e) => setMethod(e.target.value)}
+        />
+
+        <label htmlFor="rating">Rating:</label>
+        <input
+          type="number"
+          id="rating"
+          value={rating}
+          onChange={(e) => setRating(e.target.value)}
+        />
+
+        <button>Update Smoothie Recipe</button>
+
+        {/* {formError && <p className="error">{formError}</p>} */}
+      </form>
+    </div>
+  );
+};
+
+export default Update;
